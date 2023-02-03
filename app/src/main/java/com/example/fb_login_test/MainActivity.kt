be2ed1a5feb41c0633ad69066d3aacf6f8f29ff0
minju1459 <76741702+minju1459@users.noreply.github.com>
2023-02-04 01:04:09 +0900

    화면 완성

--- app/src/main/java/com/example/fb_login_test/MainActivity.kt
+++ app/src/main/java/com/example/fb_login_test/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.example.fb_login_test
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
 import android.widget.Button
+import android.widget.DatePicker
 import android.widget.ImageView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import java.util.Calendar
+import java.util.GregorianCalendar
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,6 +26,30 @@ class MainActivity : AppCompatActivity() {
 
         val writeButton=findViewById<ImageView>(R.id.writeBtn)
         writeButton.setOnClickListener{
+
+            val mDialogView=LayoutInflater.from(this).inflate(R.layout.custom_dialog,null)
+            val mBuilder=AlertDialog.Builder(this)
+                .setView(mDialogView)
+
+            val mAlertDialog=mBuilder.show()
+
+            val DateSelectBtn=mAlertDialog.findViewById<Button>(R.id.dateSelectBtn)
+
+            DateSelectBtn?.setOnClickListener{
+
+                val today=GregorianCalendar()
+                val year:Int=today.get(Calendar.YEAR)
+                val month:Int=today.get(Calendar.MONTH)
+                val date:Int=today.get(Calendar.DATE)
+
+                val dlg=DatePickerDialog(this,object:DatePickerDialog.OnDateSetListener{
+                    override fun onDateSet(view:DatePicker?,year:Int,month:Int,dayOfMonth:Int) {
+                        Log.d("main","${year},${month+1},${dayOfMonth}")
+                        DateSelectBtn.setText("${year}.${month+1}.${dayOfMonth}")
+                    }
+                }, year,month,date)
+                dlg.show()
+            }
 
         }
 
